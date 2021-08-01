aa41427f54c758a0809e22dcd1f3e9ea4b9338f4
Cody Brouwers <clbrouwers@gmail.com>
2021-08-01 20:49:46 +0900

    Update home page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,4 +1,6 @@
-import { ClapButton } from "components/ClapButton";
+import { useState } from "react";
+import { ClapButton, BackgroundScoreChart, IScore, INITIAL_SCORE_DATA } from "components";
+import { useDebounceFunction, useTrackAnalytics } from "hooks";
 
 // == Types ================================================================
 
@@ -6,6 +8,32 @@ import { ClapButton } from "components/ClapButton";
 
 // == Component ============================================================
 
-export default function Like() {
-  return <ClapButton />;
+export default function Home() {
+  const trackAnalytics = useTrackAnalytics();
+  const [clickCount, setClickCount] = useState(0);
+  const [data, setData] = useState<IScore[]>(() => INITIAL_SCORE_DATA);
+
+  const debouncedSetData = useDebounceFunction((count: number) => {
+    setData((previousData) => {
+      return [...previousData, { timestamp: new Date(), count }];
+    });
+  }, 200);
+
+  const incrementClickCount = () => {
+    setClickCount((previousCount) => {
+      previousCount += 1;
+      debouncedSetData(previousCount);
+      trackAnalytics({ event: "click", meta: { count: previousCount } });
+      return previousCount;
+    });
+  };
+
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <ClapButton clickCount={clickCount} incrementClickCount={incrementClickCount} />
+      </div>
+      <BackgroundScoreChart data={data} />
+    </>
+  );
 }
