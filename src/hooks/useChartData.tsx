172a8ea88951fc9bc9dbb2a8148380a1530a413d
Cody Brouwers <clbrouwers@gmail.com>
2021-08-03 19:34:36 +0900

    Extract chart data manipulation to a custom hook

---
+++ src/hooks/useChartData.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { TChartData } from "components";
+import { useDebounceFunction } from "hooks/useDebounceFunction";
+
+// == Types ================================================================
+
+// == Constants ============================================================
+
+// == Exports ==============================================================
+
+export function useChartData(user: IUser, clickCount: number) {
+  const [chartData, setChartData] = useState<TChartData>([]);
+
+  useEffect(() => {
+    // Set initial chart data after user is loaded
+    setChartData([{ id: user.id, label: user.name, data: [{ timestamp: new Date(), count: 0, user }] }]);
+  }, [user]);
+
+  const debouncedSetData = useDebounceFunction((data: { count: number; user: IUser }) => {
+    setChartData((previousChartData) => {
+      return previousChartData.map((chart) => {
+        if (chart.id !== data.user.id) return chart;
+
+        return {
+          ...chart,
+          data: [...chart.data, { timestamp: new Date(), ...data }],
+        };
+      });
+    });
+  }, 100);
+
+  useEffect(() => {
+    if (!clickCount) return;
+
+    debouncedSetData({ count: clickCount, user });
+  }, [clickCount, debouncedSetData, user]);
+
+  return chartData;
+}
