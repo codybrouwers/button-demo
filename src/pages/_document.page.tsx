ff1e59db73fe678cb4a56077fbc35d60a341d4a2
Cody Brouwers <clbrouwers@gmail.com>
2021-08-05 18:45:48 +0900

    Remove background colors after page has loaded

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -35,10 +35,16 @@ export default class AppDocument extends Document {
                 (function(){
                   if (!window.localStorage) return;
 
+                  // Apply saved theme background on first paint
                   if (window.localStorage.getItem('theme') === 'dark') {
                     document.documentElement.style.background = '#000';
                     document.body.style.background = '#000';
                   };
+                  // Remove theme background on page load so React can take over themeing
+                  window.addEventListener('load', (event) => {
+                    document.documentElement.style.background = null;
+                    document.body.style.background = null;
+                  });
                 })()
               `,
             }}
