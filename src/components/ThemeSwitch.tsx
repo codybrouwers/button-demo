1953702aed8d39595fb6102603a1adce0ea72fc0
Cody Brouwers <clbrouwers@gmail.com>
2021-08-09 20:37:24 +0900

    Update header icon sizes

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -20,23 +20,27 @@ export function ThemeSwitch({ theme, setTheme }: IProps) {
   return (
     <Select
       pure
+      dropdownStyle={{ overflow: "hidden" }}
       icon={theme === "light" ? Sun : Moon}
+      scale={1 / 2}
+      style={{ minWidth: "auto" }}
       value={theme}
       onChange={(selectedTheme) => setTheme(selectedTheme as TTheme)}
     >
       <Select.Option value="dark">
         <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-          <Moon size={20} />
+          <Moon size={15} />
           <Text style={{ marginLeft: 10 }}>Dark</Text>
         </div>
       </Select.Option>
       <Select.Option value="light">
         <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-          <Sun size={20} />
+          <Sun size={15} />
           <Text style={{ marginLeft: 10 }}>Light</Text>
         </div>
       </Select.Option>
     </Select>
   );
 }
+
 // == Styles ===============================================================
