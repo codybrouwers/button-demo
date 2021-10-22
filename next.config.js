05a5ba24e663114818b9d5f76b54608dfba91fa9
Cody Brouwers <clbrouwers@gmail.com>
2021-10-23 05:52:08 +0900

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
-const bundleAnalyzerPlugin = require("@next/bundle-analyzer");
-const { withPlugins } = require("next-compose-plugins");
+// const bundleAnalyzerPlugin = require("@next/bundle-analyzer");
+// const { withPlugins } = require("next-compose-plugins");
 
 // == Constants ============================================================
 
-const bundleAnalyze = bundleAnalyzerPlugin({
-  enabled: process.env.ANALYZE === "true",
-});
+// const bundleAnalyze = bundleAnalyzerPlugin({
+//   enabled: process.env.ANALYZE === "true",
+// });
 
 // == Exports ==============================================================
 
-module.exports = withPlugins([bundleAnalyze], {
+module.exports = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   // pageExtensions: ["page.tsx", "api.ts"],
   eslint: {
     dirs: ["src"],
   },
-});
+};
