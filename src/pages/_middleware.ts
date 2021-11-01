26f2d5d802980dbe04b5b75dc18938542d1805fb
Cody Brouwers <clbrouwers@gmail.com>
2021-11-02 03:18:26 +0900

    Delete middleware

--- src/pages/_middleware.ts
+++
@@ -1,5 +0,0 @@
-import { NextFetchEvent, NextResponse } from "next/server";
-
-export function middleware(event: NextFetchEvent) {
-  return event.respondWith(NextResponse.rewrite("https://www.google.com"));
-}
